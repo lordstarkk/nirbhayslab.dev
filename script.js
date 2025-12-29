ca5e100adea23c47a558d61fa8bce29e78057c6c
Lord Stark <strangexstark@gmail.com>
2025-12-29 22:32:04 +0530

    feat(identity): add identity section with profile card

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ footer_year.textContent = currentYear;
 
 // Version number
 const version_number = document.getElementById("versionNumber");
-const VERSION = "v0.2.5";
+const VERSION = "v0.3.5";
 version_number.textContent = VERSION;
 
 // Smooth scrolling for internal links
